bed9ceaf58477f5a9265e8515f319c678ef6963c
Maycem Saad <saadmaycem99@gmail.com>
2024-03-04 09:09:12 +0100

    updates

---
+++ .history/src/Repository/CategoryRepository_20240304090845.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Repository\Actualite;
+
+use App\Entity\Actualite\Categorie; // Assurez-vous que le chemin vers votre entité Categorie est correct
+use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+
+/**
+ * @extends ServiceEntityRepository<Categorie>
+ *
+ * @method Categorie|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Categorie|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Categorie[]    findAll()
+ * @method Categorie[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CategoryRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Categorie::class); // Assurez-vous que Categorie est la bonne classe d'entité
+    }
+
+//    /**
+//     * @return Category[] Returns an array of Category objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('c.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Category
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+public function findAllWithSorting(?string $sortBy = null, ?string $order = 'ASC'): array
+{
+    $queryBuilder = $this->createQueryBuilder('c');
+
+    if ($sortBy) {
+        // Add sorting criteria if provided
+        $queryBuilder->orderBy('c.' . $sortBy, $order);
+    }
+
+    return $queryBuilder->getQuery()->getResult();
+}
+
+}
